48550fc2d3a4af9334a61a90353d676b4f6930ba
Kamil Krampa <kkrampa@soldevelo.com>
2015-01-21 19:05:23 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,21 @@
+name := "SbtStarter"
+
+version := "1.0"
+
+scalaVersion := "2.11.4"
+
+scalacOptions ++= Seq("-deprecation", "-feature")
+
+libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.3.7"
+
+libraryDependencies +=  "org.scalaj" %% "scalaj-http" % "1.1.0"
+
+Revolver.settings
+
+libraryDependencies += "com.github.nscala-time" %% "nscala-time" % "1.4.0"
+
+libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.3.7"
+
+libraryDependencies += "com.typesafe.akka" %% "akka-slf4j" % "2.3.7"
+
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.1.2"
---
+++ src/main/scala/MainAkka.scala
@@ -0,0 +1,81 @@
+import java.io.PrintWriter
+
+import akka.actor._
+import akka.util.Timeout
+
+import scala.concurrent.{Await, Future}
+import scalaj.http.{HttpOptions, Http}
+import scala.concurrent.duration._
+import akka.pattern.ask
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object MainActor {
+  case class Init(url: String, depth: Int)
+  case class Resp(links: List[String])
+}
+
+
+class WorkerActor extends Actor {
+
+
+  implicit val timeout = Timeout(100.seconds)
+
+  import MainActor._
+  def receive: Receive = {
+    case Init(url, depth) =>
+      val pattern = "<a href=\"([^\"]*)\"".r
+      //println(url)
+      val links: List[String] = (pattern.findAllIn(Http(url).option(HttpOptions.readTimeout(1000)).asString.body).matchData map (m => m.group(1))).map(
+        x => if (x.contains("http://") || x.contains("https://")) x else url + "/" + x
+      ).toList
+      //println("koniec")
+      if(depth == 0) {
+        sender() ! links
+      } else {
+        val futures = for(link <- links) yield context.actorOf(Props[WorkerActor]) ? Init(link, depth - 1)
+        val result = Await.result(Future.sequence(futures), timeout.duration).asInstanceOf[List[List[String]]]
+        sender() ! (links :: result).flatten
+      }
+
+  }
+}
+
+class MainActor extends Actor {
+  import MainActor._
+  import scala.util.{Success, Failure}
+
+  def receive: Receive = {
+    case Init(url, depth) =>
+      implicit val timeout = Timeout(100.seconds)
+
+      val worker = context.actorOf(Props[WorkerActor], "worker")
+      val future = worker.ask(Init(url, depth)).mapTo[List[String]]
+      future.onComplete {
+        case Success(lista) =>
+          val sortedResult = lista.groupBy(x => x).map(x => (x._1, x._2.length)).toList.sortBy(_._2)(Ordering[Int].reverse).mkString(System.lineSeparator())
+          println(sortedResult)
+          val pw = new PrintWriter("wynik.txt")
+          pw.write(sortedResult)
+          pw.close()
+          context.system.shutdown()
+        case Failure(_) => context.system.shutdown()
+      }
+
+  }
+}
+
+object MainAkka {
+
+  import MainActor._
+
+  def main(args: Array[String]): Unit = {
+      val url = args(0)
+      val depth = args(1).toInt
+
+    val sys = ActorSystem("system")
+    val main = sys.actorOf(Props[MainActor], "Main")
+    main ! Init(url, depth)
+	}
+
+}
